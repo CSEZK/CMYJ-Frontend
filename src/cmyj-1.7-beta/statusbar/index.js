e730f6e3f5add7919c6474a4e2f6c72999d19e3e
CSEZK <169969378+CSEZK@users.noreply.github.com>
2026-07-23 22:23:19 +0800

    恢复主角身份条目预览

--- src/cmyj-1.7-beta/statusbar/index.js
+++ src/cmyj-1.7-beta/statusbar/index.js
@@ -1,7 +1,7 @@
 import ORIGINAL_TONGCHENG_CHARACTER_ADAPTATIONS from './original-tongcheng-character-adaptations.json';
 
 const STATUSBAR_ID = 'canming-afterglow-statusbar';
-const STATUSBAR_VERSION = '1.7.0-beta.9';
+const STATUSBAR_VERSION = '1.7.0-beta.10';
 const STORAGE_PREFIX = 'canming-afterglow-statusbar:';
 const VARIABLE_EDITOR_FILE = '变量修改器.js';
 const CHARACTER_GENERATOR_FILE = '万象生成器.js';
